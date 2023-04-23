472f5195b153c0f4d972073cd7da2ad5b8026862
OlgaNiem <102143760+OlgaNiem@users.noreply.github.com>
2023-04-23 17:24:54 +0300

    update

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,13 @@
+import LoginForm from '@/components/LoginForm'
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <LoginForm />
+
+    </div>
+  )
+}
+
+export default index
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,13 @@
+import RegisterForm from '@/components/RegisterForm'
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <RegisterForm />
+
+    </div>
+  )
+}
+
+export default index
