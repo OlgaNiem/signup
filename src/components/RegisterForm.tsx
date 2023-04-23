31e9c64adc7154ec05d7fd7b6179ee284eb5e744
OlgaNiem <102143760+OlgaNiem@users.noreply.github.com>
2023-04-23 18:32:00 +0300

    update

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import style from '../styles/Home.module.css'
 
 const RegisterForm = () => {
-  return (
-    <div>
-        <h1>RegisterForm</h1>
+    return (
+        <div className={style.body}>
+            <div className={style.loginBox}>
+                <h2>Sign Up</h2>
+            <form>
+                <div className={style.userBox}>
+                    <input type="text" name="" />
+                    <label>Username</label>
+                </div>
+                <div className={style.userBox}>
+                    <input type="text" name="" />
+                    <label>Email</label>
+                </div>
+                <div className={style.userBox}>
+                    <input type="password" name="" />
+                    <label>Password</label>
+                </div>
+                <button className={style.btn} type="submit">Submit</button>
+            </form>
+            </div>
         </div>
-  )
-}
+      )
+    }
 
 export default RegisterForm
